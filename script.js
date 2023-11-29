390516c057e2c56199a1996310a3b35d69f61ef1
Luiz Fernando <112594037+LuizFernandoFerri@users.noreply.github.com>
2023-11-29 18:50:13 -0300

    att

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 AOS.init();
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Função para pausar o vídeo quando estiver fora da tela
+function pauseVideoOnScroll() {
+  var video = document.querySelector('video');
+  if (!isElementInViewport(video)) {
+    video.pause();
+  }
+}
+
+// Evento de rolagem para checar quando o vídeo está visível
+window.addEventListener('scroll', pauseVideoOnScroll);
